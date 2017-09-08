2b3c20e9bbff31035054adb8eb6b7cb0922fd4ce
Iris Seaman <irisrseaman@gmail.com>
2017-09-08 00:00:46 -0600

    added simplify_path and walk_path through time with a speed

--- planner.py
+++ planner.py
@@ -5,7 +5,6 @@ ax = None
 	
 #(2000, 3.0, 10000, 1.)
 def optimize_path(x1, y1, x2, y2, orig_path, iters, std):
-	SCALE = 1000
 	points = orig_path[:]
 	inner_pt_cnt = len(points)-2
 	if inner_pt_cnt == 0:
@@ -27,7 +26,6 @@ def optimize_path(x1, y1, x2, y2, orig_path, iters, std):
 		curr_dist = dist(pre_pt, pt) + dist(pt, next_pt)
 		
 
-		#ax.scatter( ad_pt[0] * 1000, ad_pt[1]  * 1000)
 		back_int_x, back_int_y, back_intersection_indicators = line_intersect( 
 	            pre_pt[0], 
 	            pre_pt[1], 
@@ -53,26 +51,70 @@ def optimize_path(x1, y1, x2, y2, orig_path, iters, std):
 
 	return points
 
+def walk_path(path, speed, times):
+	distances_from_start = [0.0]*len(path)
+	for i in xrange(1, len(path)):
+		distances_from_start[i] = distances_from_start[i-1]+ dist(path[i-1], path[i])
+
+	locations = [-1]*len(times)
+	locations[0] = path[0]
+
+	for time_i, t in enumerate(times):
+		desired_distance = t * speed
+		used_up_time = False
+		for i in xrange (1, len(path)):
+			prev = path[i-1] 
+			cur = path[i] 
+			dist_to_prev = dist(prev, cur)
+			if distances_from_start[i] >= desired_distance:
+				#we overshot, the location is beween i-1 and i
+				overshoot = distances_from_start[i] - desired_distance
+				past_prev = dist_to_prev - overshoot
+				frac = past_prev / dist_to_prev
+				locations[time_i] = [prev[0] * (1.0 - frac) + cur[0] * frac, prev[1] * (1.0 - frac) + cur[1] * frac]
+				used_up_time = True
+				break
+		if not used_up_time:
+			locations[time_i] = path[-1]
+
+	return locations
+
+
+
+
 def simplify_path(x1, y1, x2, y2, orig_path):
 	points = [orig_path[0]]
-	for i in xrange(1, len(orig_path)-2):
+	for i in xrange(1, len(orig_path)-1):
 		last_pt = points[-1]
 		next_pt = orig_path[i]
-		bnt_x, int_y, intersection_indicators = line_intersect( 
+		int_x, int_y, intersection_indicators = line_intersect( 
+	            next_pt[0], 
+	            next_pt[1],
 	            last_pt[0], 
 	            last_pt[1], 
-	            next_pt[0], 
-	            next_pt[1], 
 	            x1, y1, x2, y2)
 		if intersection_indicators.any():
+			points.append(orig_path[i-1])
 			points.append(orig_path[i])
 
 	points.append(orig_path[-1])
 	return points
 
 
+def run_rrt_opt(start_pt, goal_pt, x1, y1, x2, y2):
+	path = run_rrt( start_pt, goal_pt, x1, y1, x2, y2)
+	iters = 10000
+	std = 1.0/500
+	opt_path = optimize_path(x1, y1, x2, y2, path, iters, std)
+	sim_path = simplify_path(x1, y1, x2, y2, opt_path)
 
-def exp_dist(x1, y1, x2, y2, start_pt, goal_pt):
+	times = np.arange(0, 600, 20) #600 seconds (5 mintues) 30 20-sec intervals
+	walking_path = walk_path(sim_path, 1.5/600.0, times)
+	return walking_path
+
+
+
+def get_distanes(x1, y1, x2, y2, start_pt, goal_pt):
     distances = []
     for i in xrange(100):
 		path = run_rrt( start_pt, goal_pt, x1, y1, x2, y2)
@@ -82,13 +124,13 @@ def exp_dist(x1, y1, x2, y2, start_pt, goal_pt):
 	#print(np.mean(distances))
 
 
-
 if __name__ == '__main__':
     polygons = load_polygons( "./paths.txt" )
     x1, y1, x2, y2 = polygons_to_segments( polygons )
     
-    start_pt = np.atleast_2d( [182.0/500,12.0/500] )
-    goal_pt = np.atleast_2d( [409.0/500,353.0/500] )
+    
+    start_pt = np.atleast_2d( [182.0/1000,12.0/1000] )
+    goal_pt = np.atleast_2d( [409.0/1000,353.0/1000] )
 
     x1, y1, x2, y2 = polygons_to_segments( polygons )
 
@@ -101,35 +143,37 @@ if __name__ == '__main__':
     ax = fig.add_subplot(1, 1, 1)
 
     #c = np.linspace(0, 10, np_paths.shape[0])
-    img = plt.imread("./cnts_inv.png")
-    ax.imshow(img)
+    #img = plt.imread("./cnts_inv.png")
+    #ax.imshow(img)
 
-    scale = 1000
+    scale = 1
 
     # plot original RRT path
     for i in range( 0, len(path)-1 ):
         ax.plot( [ path[i][0] * scale, path[i+1][0] * scale ], [ path[i][1] * scale, path[i+1][1] * scale], 'b' )
      
     iters = 10000
-    std = 1.0/500          
+    std = 1.0/500
+    #new_path =run_rrt_opt(start_pt, goal_pt, x1, y1, x2, y2)     
     new_path = optimize_path(x1, y1, x2, y2, path, iters, std)
 
-    # plot optimized path
-    for i in range( 0, len(new_path)-1 ):
-        ax.plot( [ new_path[i][0] * scale, new_path[i+1][0] * scale ], [ new_path[i][1] * scale, new_path[i+1][1] * scale], 'red' )
+    #plot optimized path
+    # for i in range( 0, len(new_path)-1 ):
+    #    ax.plot( [ new_path[i][0] * scale, new_path[i+1][0] * scale ], [ new_path[i][1] * scale, new_path[i+1][1] * scale], 'green' )
      
-    sim_path = simplify_path(x1, y1, x2, y2, path)
+    # sim_path = simplify_path(x1, y1, x2, y2, new_path)
 
-    # plot simplification from original RRT path
-    for i in range( 0, len(sim_path)-1 ):
-        ax.plot( [ sim_path[i][0] * scale, sim_path[i+1][0] * scale ], [ sim_path[i][1] * scale, sim_path[i+1][1] * scale], 'limegreen' )
+    # # plot simplification from original RRT path
+    # for i in range( 0, len(sim_path)-1 ):
+    #     ax.plot( [ sim_path[i][0] * scale, sim_path[i+1][0] * scale ], [ sim_path[i][1] * scale, sim_path[i+1][1] * scale], 'limegreen' )
 
     sim_path = simplify_path(x1, y1, x2, y2, new_path)
 
     # plot simplification of optimized path
     for i in range( 0, len(sim_path)-1 ):
-        ax.plot( [ sim_path[i][0] * scale, sim_path[i+1][0] * scale ], [ sim_path[i][1] * scale, sim_path[i+1][1] * scale], 'gold' )
-     
+        ax.plot( [ sim_path[i][0] * scale, sim_path[i+1][0] * scale ], [ sim_path[i][1] * scale, sim_path[i+1][1] * scale], 'red' )
+     	#ax.scatter( sim_path[i][0] * scale, sim_path[i][1]  * scale, color="purple")
+
 
     ax.scatter( start_pt[0,0] * scale, start_pt[0,1]  * scale)
     ax.scatter( goal_pt[0,0] * scale, goal_pt[0,1] * scale)
@@ -138,7 +182,12 @@ if __name__ == '__main__':
     for i in xrange(x1.shape[0]):
     	ax.plot( [ x1[i,0] * scale, x2[i,0] * scale ], [ y1[i,0] * scale, y2[i,0] * scale], 'black' )
 
-    # distances = exp_dist(x1, y1, x2, y2, start_pt, goal_pt)
+
+
+
+
+
+    # distances = get_distances(x1, y1, x2, y2, start_pt, goal_pt)
     # print(np.mean(distances) * 500)
     # print(np.max(distances) * 500)
     # print(np.min(distances) * 500)
